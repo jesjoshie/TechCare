ed9d18377df4afc106a79e9791af18061de2882c
Joshie <kalilinux19701375@gmail.com>
2020-10-30 10:11:15 +0800

    edited the main  interface

--- app/src/main/java/com/example/techcare/MainActivity.kt
+++ app/src/main/java/com/example/techcare/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.techcare
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Button2 = findViewById<View>(R.id.button1) as Button
+
+
+        Button2.setOnClickListener {
+            val int1 = Intent(this@MainActivity, Signup::class.java)
+            startActivity(int1)
+        }
     }
 }
